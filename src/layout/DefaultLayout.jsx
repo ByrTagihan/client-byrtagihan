a2f56166e57cefc644fb23b4ad448af30e0e1106
Azizi Khoiri <ibnuljefry99@gmail.com>
2023-07-25 21:50:04 +0700

    update token expired

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { useNavigate } from 'react-router-dom';
 
 const DefaultLayout = () => {
+let navigate = useNavigate();
+const token = localStorage.getItem("token"); 
+const parsedToken = JSON.parse(atob(token.split('.')[1])); 
+const expirationTimestamp = parsedToken.exp; 
+
+const isTokenExpired = () => {
+  const currentTime = Math.floor(Date.now() / 1000); 
+  return currentTime > expirationTimestamp;
+};
+
+const handleLogout = () => {
+  console.log('Token expired. Logging out...');
+  navigate("/");
+  setTimeout(() => {
+    window.location.reload();
+  }, 1500);
+  localStorage.clear();
+};
+
+useEffect(() => {
+  const checkTokenExpiration = setInterval(() => {
+    if (isTokenExpired()) {
+      handleLogout();
+      clearInterval(checkTokenExpiration);
+    }
+  }, 1000);
+  return () => clearInterval(checkTokenExpiration); 
+}, []);
   return (
     <div>
       <AppSidebar />
